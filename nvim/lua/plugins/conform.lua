c6c7726799f542e3c8f886e741112cdec3f1323c
Michael Richardson <8782+mrichardson03@users.noreply.github.com>
2025-09-13 19:27:54 -0400

    feat: add lazyvim

---
+++ nvim/lua/plugins/conform.lua
@@ -0,0 +1,10 @@
+return {
+  "stevearc/conform.nvim",
+  opts = {
+    formatters_by_ft = {
+      hcl = { "terragrunt_hclfmt", "hc", stop_after_first = true },
+      python = { "ruff_format", "ruff_organize_imports" },
+      terraform = { "tofu_fmt", "terraform_fmt", stop_after_first = true },
+    },
+  },
+}
---
+++ nvim/lua/plugins/lspconfig.lua
@@ -0,0 +1,15 @@
+return {
+  "neovim/nvim-lspconfig",
+  opts = {
+    servers = {
+      ruff = {
+        init_options = {
+          settings = {
+            logLevel = "info",
+          },
+        },
+      },
+      terraformls = {},
+    },
+  },
+}
