e10577c8d3eb614f2eb6bcab71d9bd4ad98553c8
Michael Richardson <8782+mrichardson03@users.noreply.github.com>
2025-09-14 16:37:25 -0400

    feat: update lazyvim

--- nvim/lua/plugins/conform.lua
+++ nvim/lua/plugins/conform.lua
@@ -2,7 +2,7 @@ return {
   "stevearc/conform.nvim",
   opts = {
     formatters_by_ft = {
-      hcl = { "terragrunt_hclfmt", "hc", stop_after_first = true },
+      hcl = { "terragrunt_hclfmt", "hcl", stop_after_first = true },
       python = { "ruff_format", "ruff_organize_imports" },
       terraform = { "tofu_fmt", "terraform_fmt", stop_after_first = true },
     },
